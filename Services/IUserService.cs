43b38783804a5f0582e7a4d286efc0e67e7f042b
devinayolanda <510043@student.fontys.nl>
2023-01-09 00:46:06 +0100

    test github action

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -7,5 +7,6 @@ public interface IUserService
     Task<UserEntity?> CreateUser(UserEntity data);
     Task<UserEntity?> EditUser(UserEntity data);
     Task<UserEntity?> GetUser(int id);
+    // Task<UserEntity?> GetUsername(String username);
     Task<bool> DeleteUser(int id);
 }
